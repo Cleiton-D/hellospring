85f5868a78df8ad89d724e87f13bb90932740f1c
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-03-28 01:17:45 -0400

    .

--- src/main/java/br/com/cleitonkiper/testek8/HelloController.java
+++ src/main/java/br/com/cleitonkiper/testek8/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
 	
-	@RequestMapping("/health")
+	@RequestMapping("/")
 	public String index() {
 		return "health";
 	}
